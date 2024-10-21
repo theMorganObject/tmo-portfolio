de548ffe746082bc2c6258e8a17d121c1c1a4f11
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-10-21 13:39:13 +0100

    updated nav

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,7 +6,7 @@ const colors = ['#5c940d', '#1864ab', '#e03131', '#343a40'];
 
 export default function About() {
   return (
-    <section className='relative min-h-screen bg-red-13 font-sans'>
+    <section className='min-h-screen bg-red-13 font-sans'>
       <Accordion data={morganObjects} colors={colors} />
     </section>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,7 @@ export default function Home() {
 
   return (
     <section className='min-h-screen bg-gray-9 font-sans flex flex-col items-center'>
-      <p className='text-sm text-red-3 mt-32 sm:text-base md:text-lg lg:text-xl'>
-        Front End Developer | Scrum Master
-      </p>
-      <h1 className='font-reey text-2xl text-lime-6 leading-loose mt-6 2xs:text-3xl sm:text-4xl md:text-5xl md:mt-10 lg:text-6xl lg:mt-12'>
+      <h1 className='font-reey text-2xl text-lime-6 leading-loose mt-32 2xs:text-3xl sm:text-4xl md:text-5xl lg:text-6xl'>
         {fullName}
       </h1>
       <ul className='grid grid-rows-4 grid-cols-20 xs:grid-cols-12 gap-y-4 2xs:text-xs px-1.5 mt-12 sm:text-base md:text-xl md:mt-20 lg:text-2xl lg:mt-24'>
