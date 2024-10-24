4931660d9fce16aca1ebcda79606dad22ef0563d
Morgan OShaughnessey <123529383+theMorganObject@users.noreply.github.com>
2024-10-24 14:19:34 -0500

    nav button styling

--- src/components/UI/Nav.tsx
+++ src/components/UI/Nav.tsx
@@ -1,18 +1,37 @@
 import Link from 'next/link';
+import styles from './nav.module.css';
 
 export default function Nav() {
   return (
     <nav aria-label='Main navigation'>
-      <ul className='flex gap-16 text-blue-3 text-sm underline underline-offset-[3px] decoration-1 cursor-pointer focus:outline-none focus:ring-2 focus:ring-red-5 sm:text-base md:text-lg lg:text-xl'>
-        <Link href='/' className='hover:text-red-3' aria-label='Home'>
-          Home
-        </Link>
-        <Link href='/about' className='hover:text-red-3' aria-label='About'>
-          About
-        </Link>
-        <Link href='/contact' className='hover:text-red-3' aria-label='Contact'>
-          Contact
-        </Link>
+      <ul className='flex gap-16 items-center text-sm cursor-pointer focus:outline-none focus:ring-2 focus:ring-red-5 sm:text-base md:text-lg lg:text-xl'>
+        <li className='relative'>
+          <Link
+            href='/'
+            className={`relative z-10 px-6 py-3 ${styles.navItem} ${styles.red}`}
+            aria-label='Home'
+          >
+            Home
+          </Link>
+        </li>
+        <li className='relative'>
+          <Link
+            href='/about'
+            className={`relative z-10 px-6 py-3 ${styles.navItem} ${styles.blue}`}
+            aria-label='About'
+          >
+            About
+          </Link>
+        </li>
+        <li className='relative'>
+          <Link
+            href='/contact'
+            className={`relative z-10 px-6 py-3 ${styles.navItem} ${styles.green}`}
+            aria-label='Contact'
+          >
+            Contact
+          </Link>
+        </li>
       </ul>
     </nav>
   );
